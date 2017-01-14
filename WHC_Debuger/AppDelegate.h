29587415c5353ad3b25ed52fdaf8fbbe58fdcb5e
netyouli <712641411@qq.com>
2017-01-14 17:38:16 +0800

    Initial Commit

---
+++ WHC_Debuger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHC_Debuger
+//
+//  Created by WHC on 17/1/14.
+//  Copyright © 2017年 WHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHC_Debuger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHC_Debuger
+//
+//  Created by WHC on 17/1/14.
+//  Copyright © 2017年 WHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
